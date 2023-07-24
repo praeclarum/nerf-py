ce393272184c45eed225e53dbc6a80760224974a
Frank A. Krueger <fak@praeclarum.org>
2023-07-23 18:01:09 -0700

    Correct alpha compositing

--- renderer.py
+++ renderer.py
@@ -37,17 +37,43 @@ def render(radiance,
     sample_densities, sample_colors = radiance(
         sample_positions.view(num_rays * num_samples_per_ray, 3),
         sample_dirs.view(num_rays * num_samples_per_ray, 3))
-    
-    # Integrate the density and color along the ray
     sample_densities = sample_densities.view(num_rays, num_samples_per_ray)
     sample_colors = sample_colors.view(num_rays, num_samples_per_ray, 3)
-    sample_total_densities = torch.sum(sample_densities, dim=1)
-    sample_weighted_colors = sample_colors * sample_densities.view(num_rays, num_samples_per_ray, 1)
-    sample_total_colors = torch.sum(sample_weighted_colors, dim=1)
-    pixel_colors = sample_total_colors / sample_total_densities.view(num_rays, 1)
-    pixel_colors = pixel_colors.view(height, width, 3)
+    
+    # Integrate the density and color along the ray
+    mean_sample_separation = (z_far - z_near) / num_samples_per_ray
+    colors = alpha_composite(sample_densities, sample_colors, mean_sample_separation, sample_distances.view(num_rays, num_samples_per_ray))
+    colors = colors.view(height, width, 4)
+    return colors
 
-    return pixel_colors
+def alpha_composite(sample_densities, sample_colors, mean_sample_separation, sample_distances):
+    """
+    Composite colors using alpha blending
+
+    sample_density shape (num_rays, num_samples_per_ray)
+    sample_colors shape (num_rays, num_samples_per_ray, 3)
+    mean_sample_separation is a scalar
+    sample_distances shape (num_rays, num_samples_per_ray)
+    """
+    num_rays, num_samples_per_ray = sample_densities.shape
+    sample_separations = sample_distances[:, 1:] - sample_distances[:, :-1]
+    scaled_densities = sample_densities[:, :-1] * sample_separations
+    alphas = 1.0 - torch.exp(-scaled_densities)
+    # print(alphas[700*500*11//2])
+    composite_alpha = 1.0 - torch.exp(-sample_densities[:, -1:] * mean_sample_separation)
+    composite_color = sample_colors[:, -1]
+    for i in range(num_samples_per_ray - 2, -1, -1):
+        alpha_i = torch.unsqueeze(alphas[:, i], 1)
+        one_minus_alpha_i = 1.0 - alpha_i
+        # raise Exception(f"alpha_i.shape = {alpha_i.shape}")
+        composite_color = alpha_i * sample_colors[:, i, :] + one_minus_alpha_i * composite_color
+        # raise Exception(f"composite_color.shape = {composite_color.shape}")
+        composite_alpha = alpha_i + one_minus_alpha_i * composite_alpha
+        # print(composite_alpha[700*500//2+700])
+        # raise Exception(f"composite_alpha.shape = {composite_alpha.shape}")
+    # raise Exception(f"sample_distances.shape = {sample_distances.shape}")
+    composite_rgba = torch.cat([composite_color, composite_alpha], dim=1)
+    return composite_rgba
 
 def sample_binned_uniform_distances(t_min, t_max, num_rays, num_samples_per_ray):
     """
@@ -105,7 +131,7 @@ def sphere_radiance(position, view_direction):
     center = torch.tensor([0.0, 0.0, -5.0])
     radius = 1.0
     distance_to_surface = torch.norm(position - center.view(1, 3), dim=1, keepdim=True) - radius
-    density = torch.clamp(torch.relu(-distance_to_surface), 0.0, 1.0)
+    density = torch.clamp(torch.relu(-distance_to_surface), 0.0, 0.1) * 10
     color = torch.clamp(position - center.view(1, 3), 0, 1)
     return (density, color)
 
@@ -114,8 +140,9 @@ def show_image(image):
     plt.show()
 
 if __name__ == "__main__":
-    for angle in range(30, 61, 15):
-        y_rot_rads = math.radians(angle-45)
+    # for angle in [0]:
+    for angle in [-30, 0, 30]:
+        y_rot_rads = math.radians(angle)
         camera_local_to_world=torch.eye(4)
         camera_local_to_world[0, 0] = math.cos(y_rot_rads)
         camera_local_to_world[0, 2] = math.sin(y_rot_rads)
